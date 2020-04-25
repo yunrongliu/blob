b313559a98e680c2880fac0399c68ba0366fe418
liucong <liucong@originaltek.com>
2020-04-25 18:03:57 +0800

    test

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ function test (name='cc') {
   console.log(name)
 }
 
-if(navigator.serviceWorker) {
-  window.addEventListener('DOMContentLoaded',()=> {
-    navigator.serviceWorker.register('/blob/sw.js')
-  })
-}
+// if(navigator.serviceWorker) {
+//   window.addEventListener('DOMContentLoaded',()=> {
+//     navigator.serviceWorker.register('/blob/sw.js')
+//   })
+// }
 
 test()

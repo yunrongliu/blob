60c910a40a246c44412e92ba651acb128183fe1e
liucong <liucong@originaltek.com>
2020-04-25 17:44:20 +0800

    -- user = yunrong 提交代码

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import {info} from './test'
+console.log(info)
+
+function test (name='cc') {
+  console.log(name)
+}
+
+if(navigator.serviceWorker) {
+  window.addEventListener('DOMContentLoaded',()=> {
+    navigator.serviceWorker.register('/sw.js')
+  })
+}
+
+test()
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.js)$/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  devServer: {
+    https: true,
+    open: true,
+    inline: true,
+    contentBase: path.join(__dirname,'dist'),
+  }
+};
